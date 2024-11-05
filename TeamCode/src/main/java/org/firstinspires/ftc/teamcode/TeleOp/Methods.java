45d2f258a7a973cc9260e6fe4f6c0edeaa25e41a
American Huston <Americanhuston@gmail.com>
2024-11-05 12:50:09 -0700

    Testing Java Methods

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Methods.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+public class Methods {
+    static int Adding(int var1, int var2){
+        return var1 + var2;
+    }
+    static int Subtracting(int var1, int var2){
+        return var1 - var2;
+    }
+    static int Multiplying(int var1, int var2){
+        return var1 * var2;
+    }
+    static int Dividing(int var1, int var2){
+        return var1 / var2;
+    }
+}
