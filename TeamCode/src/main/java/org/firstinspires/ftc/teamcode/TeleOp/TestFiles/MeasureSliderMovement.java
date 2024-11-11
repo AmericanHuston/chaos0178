3cf1981c24484e462ccf7fe6f649af5b5a994169
American Huston <Americanhuston@gmail.com>
2024-11-11 14:38:15 -0700

    Added driving to slider code, using Methods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestFiles/MeasureSliderMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestFiles/MeasureSliderMovement.java
@@ -1,51 +1,76 @@
 package org.firstinspires.ftc.teamcode.TeleOp.TestFiles;
 
+import static org.firstinspires.ftc.teamcode.TeleOp.TestFiles.Methods.driving;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "MeasureSliderMovement", group = "TeleOp")
 @Config
 public class MeasureSliderMovement extends LinearOpMode {
-    public DcMotor sliderLeft = hardwareMap.get(DcMotor.class, "SliderLeft");
-    public DcMotor sliderRight = hardwareMap.get(DcMotor.class, "SliderRight");
+
+    public DcMotor SliderLeft;
+    public DcMotor SliderRight;
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        SliderLeft = hardwareMap.get(DcMotor.class, "SliderLeft");
+        SliderRight = hardwareMap.get(DcMotor.class, "SliderRight");
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
         //Don't edit code below this point
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         dashboard.updateConfig();
         waitForStart();
-        int leftPos = sliderLeft.getCurrentPosition();
-        int rightPos = sliderRight.getCurrentPosition();
+        int leftPos = SliderLeft.getCurrentPosition();
+        int rightPos = SliderRight.getCurrentPosition();
         double power = 0.35;
         //Don't edit code above this point
-        while(opModeIsActive()){
-            while (this.gamepad2.dpad_up){
-                sliderLeft.setPower(power);
-                sliderRight.setPower(power);
-                leftPos = sliderLeft.getCurrentPosition();
-                rightPos = sliderRight.getCurrentPosition();
-                telemetry.addData("leftPos", leftPos);
+        while (opModeIsActive()) {
+            while (this.gamepad2.dpad_up) {
+                SliderLeft.setPower(power);
+                SliderRight.setPower(-power);
+                driving();
+                leftPos = SliderLeft.getCurrentPosition();
+                rightPos = SliderRight.getCurrentPosition();
+                telemetry.addData("leftPos", -leftPos);
                 telemetry.addData("rightPos", rightPos);
                 telemetry.update();
             }
-            sliderRight.setPower(0.0);
-            sliderLeft.setPower(0.0);
-            while (this.gamepad2.dpad_down){
-                sliderLeft.setPower(-power);
-                sliderRight.setPower(-power);
-                leftPos = sliderLeft.getCurrentPosition();
-                rightPos = sliderRight.getCurrentPosition();
-                telemetry.addData("leftPos", leftPos);
+            driving();
+            SliderRight.setPower(0.0);
+            SliderLeft.setPower(0.0);
+            while (this.gamepad2.dpad_down) {
+                SliderLeft.setPower(-power);
+                SliderRight.setPower(power);
+                driving();
+                leftPos = SliderLeft.getCurrentPosition();
+                rightPos = SliderRight.getCurrentPosition();
+                telemetry.addData("leftPos", -leftPos);
                 telemetry.addData("rightPos", rightPos);
                 telemetry.update();
             }
-            sliderRight.setPower(0.0);
-            sliderLeft.setPower(0.0);
+            driving();
+            SliderRight.setPower(0.0);
+            SliderLeft.setPower(0.0);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestFiles/Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestFiles/Methods.java
@@ -1,6 +1,20 @@
 package org.firstinspires.ftc.teamcode.TeleOp.TestFiles;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 public class Methods {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+
     public static int Adding(int var1, int var2){
         return var1 + var2;
     }
@@ -12,5 +26,36 @@ public class Methods {
     }
     public static int Dividing(int var1, int var2){
         return var1 / var2;
+    }
+    public static void driving() {
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        double y = -gamepad1.left_stick_y / 2; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x / 2;
+        double rx = gamepad1.right_stick_x / 2;
+        if (gamepad1.right_trigger >= 0.01) {
+            y = y * 2;
+            x = x * 2;
+            rx = rx * 2;
+        }
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        if (gamepad1.left_stick_button) {
+            imu.resetYaw();
+        }
+        rotX = rotX * 1.1;
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
     }
 }
