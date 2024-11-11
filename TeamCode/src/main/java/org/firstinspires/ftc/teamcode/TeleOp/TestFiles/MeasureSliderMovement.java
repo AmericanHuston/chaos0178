a6a2ce0d4414a26be1fa2a896543ccda84a1ae3a
American Huston <Americanhuston@gmail.com>
2024-11-11 13:14:52 -0700

    fixed code to work with robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestFiles/MeasureSliderMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestFiles/MeasureSliderMovement.java
@@ -1,33 +1,48 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode.TeleOp.TestFiles;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="TestSliders", group="TeleOp")
-public class Sliders extends LinearOpMode {
-
+@TeleOp(name = "MeasureSliderMovement", group = "TeleOp")
+@Config
+public class MeasureSliderMovement extends LinearOpMode {
     public DcMotor sliderLeft = hardwareMap.get(DcMotor.class, "SliderLeft");
     public DcMotor sliderRight = hardwareMap.get(DcMotor.class, "SliderRight");
     @Override
     public void runOpMode() throws InterruptedException {
+        //Don't edit code below this point
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         dashboard.updateConfig();
         waitForStart();
+        int leftPos = sliderLeft.getCurrentPosition();
+        int rightPos = sliderRight.getCurrentPosition();
         double power = 0.35;
+        //Don't edit code above this point
         while(opModeIsActive()){
-            while (this.gamepad1.dpad_up){
+            while (this.gamepad2.dpad_up){
                 sliderLeft.setPower(power);
                 sliderRight.setPower(power);
+                leftPos = sliderLeft.getCurrentPosition();
+                rightPos = sliderRight.getCurrentPosition();
+                telemetry.addData("leftPos", leftPos);
+                telemetry.addData("rightPos", rightPos);
+                telemetry.update();
             }
             sliderRight.setPower(0.0);
             sliderLeft.setPower(0.0);
-            while (this.gamepad1.dpad_down){
+            while (this.gamepad2.dpad_down){
                 sliderLeft.setPower(-power);
                 sliderRight.setPower(-power);
+                leftPos = sliderLeft.getCurrentPosition();
+                rightPos = sliderRight.getCurrentPosition();
+                telemetry.addData("leftPos", leftPos);
+                telemetry.addData("rightPos", rightPos);
+                telemetry.update();
             }
             sliderRight.setPower(0.0);
             sliderLeft.setPower(0.0);
