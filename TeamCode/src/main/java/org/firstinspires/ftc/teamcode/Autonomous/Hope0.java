d0b6249d4facccd40b62a228adb38d1417f7018a
American Huston <Americanhuston@gmail.com>
2024-11-22 13:56:01 -0700

    Created Hope0.java, which I might be able to get working before the scrimmage. I can't promise that though.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Hope0.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Hope0", group = "Hope")
+public class Hope0 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
