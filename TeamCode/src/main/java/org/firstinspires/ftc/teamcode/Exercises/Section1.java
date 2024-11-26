9f5d02a10d02fd6cb62f03da3273e53581b2f6ea
American Huston <Americanhuston@gmail.com>
2024-11-25 23:33:43 -0700

    Completed Section1 exercises

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/Section1.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Exercises;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+@Autonomous(name = "Section1", group = "Autonomous")
+public class Section1 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        String name = "Noah";
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        dashboard.updateConfig();
+        telemetry.addData("Hello", name);
+        telemetry.update();
+    }
+}
