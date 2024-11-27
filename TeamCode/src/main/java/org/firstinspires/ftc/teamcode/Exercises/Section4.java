f4a274b1f0876831b9238a9e5e207f79f5ed062a
American Huston <Americanhuston@gmail.com>
2024-11-27 00:20:00 -0700

    Completed Section4 exercises

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/Section4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises/Section4.java
@@ -15,5 +15,16 @@ public class Section4 extends LinearOpMode {
         if (currentPos > 1.0){
             currentPos = 1.0;
         }
+        //"Crazy mode"
+
+        float prevX = this.gamepad1.right_stick_x;
+        float prevY = this.gamepad1.right_stick_y;
+        float x = prevX;
+        float y = prevY;
+        if (this.gamepad1.a){
+            x = prevY;
+            y = prevX;
+        }
+        //Do all the control stuff
     }
 }
