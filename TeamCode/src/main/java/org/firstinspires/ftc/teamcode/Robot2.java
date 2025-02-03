bfa3176c8fff184352ee90617adb9bd6061f5278
Lumennnnnnn <serackliffe@gmail.com>
2025-02-03 15:02:03 -0700

    slider below bar position adjustment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2.java
@@ -47,7 +47,7 @@ public class Robot2 {
     public static double MAX_POS     =  1.0;     // Maximum rotational position
     public static double MIN_POS     =  0.0;// Minimum rotational position
     public static int slider_above_bar_position = 1800;
-    public static int slider_below_bar_position = 1250;
+    public static int slider_below_bar_position = 1225;
     public static int shoulder_bar_position = 100;
     //public static double wrist_bar_position = 0.39;
     public static int shoulder_bar_velotity = 200;
