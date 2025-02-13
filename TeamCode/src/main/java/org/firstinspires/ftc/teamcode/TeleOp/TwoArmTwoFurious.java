aebc3835917aaee404f59ad8316a99a928da6c84
Lumennnnnnn <serackliffe@gmail.com>
2025-02-12 21:30:30 -0700

    Got the points for TeleOp fixed!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TwoArmTwoFurious.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TwoArmTwoFurious.java
@@ -19,8 +19,8 @@ public class TwoArmTwoFurious extends OpMode {
 
     private Follower follower;
     private final Pose startPose = new Pose(8, 72, 0);
-    private final Pose SpecGrab = new Pose(20, 35, Math.toRadians(180));
-    private final Pose Basket = new Pose(20, 142.5, Math.toRadians(130));
+    private final Pose SpecGrab = new Pose(13, 35, Math.toRadians(180));
+    private final Pose Basket = new Pose(15, 130, Math.toRadians(130));
     private final Pose HangLeft = new Pose( 80, 95, Math.toRadians(270));
     private final Pose HangRight = new Pose( 57, 95, Math.toRadians(270));
 
