f5ffc296b5d67458535146dda0d5117be40f8f56
Lumennnnnnn <serackliffe@gmail.com>
2025-02-18 13:21:33 -0700

    fixed the autopilot and starting pose for basket

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BasketAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BasketAuto.java
@@ -41,21 +41,21 @@ public class BasketAuto extends OpMode {
     //Other Observation Zone is 144,144
     //0 PROBABLY intersects the fully coloOther bars
     // Pose goes in this order: Pose(x,y, Radians);
-    private final Pose StartingPose = new Pose(8, 104, Math.toRadians(90));
-    private final Pose Basket = new Pose(13.5,126.5, Math.toRadians(130));
-    private final Pose OtherObservation = new Pose(120, 104, Math.toRadians(90));
-    private final Pose OtherBasket = new Pose(120, 8, Math.toRadians(135));
-    private final Pose Observation = new Pose(8,34, Math.toRadians(0));
-    private final Pose TapeHangRobot = new Pose(72,80, Math.toRadians(90));
-    private final Pose OtherTapeHangRobot = new Pose(72,32, Math.toRadians(270));
-    private final Pose littleBack = new Pose(20, 114, Math.toRadians(130));
-    private final Pose sample1  = new Pose(24.5, 128, Math.toRadians(0));
-    private final Pose sample2 = new Pose(24.5,118, Math.toRadians(0));
-    private final Pose sample3 = new Pose(45.5, 120.8, Math.toRadians(90));
-    private final Pose hangBar = new Pose(80,94, Math.toRadians(270));
-    private final Point BasketPoint = new Point(13.5, 124.5);
-    private final Point hangBarPoint = new Point(60, 99);
-    private final Point parkControlPoint = new Point(64, 122);
+    private final Pose StartingPose = new Pose(8, 102, Math.toRadians(90));
+    private final Pose Basket = new Pose(13.5,124.5, Math.toRadians(130));
+    private final Pose OtherObservation = new Pose(120, 102, Math.toRadians(90));
+    private final Pose OtherBasket = new Pose(120, 6, Math.toRadians(135));
+    private final Pose Observation = new Pose(8,32, Math.toRadians(0));
+    private final Pose TapeHangRobot = new Pose(72,78, Math.toRadians(90));
+    private final Pose OtherTapeHangRobot = new Pose(72,30, Math.toRadians(270));
+    private final Pose littleBack = new Pose(20, 112, Math.toRadians(130));
+    private final Pose sample1  = new Pose(24.5, 126, Math.toRadians(0));
+    private final Pose sample2 = new Pose(24.5,116, Math.toRadians(0));
+    private final Pose sample3 = new Pose(45.5, 118.8, Math.toRadians(90));
+    private final Pose hangBar = new Pose(80,92, Math.toRadians(270));
+    private final Point BasketPoint = new Point(13.5, 122.5);
+    private final Point hangBarPoint = new Point(60, 97);
+    private final Point parkControlPoint = new Point(64, 120);
 
     private PathChain square;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TwoArmTwoFurious.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TwoArmTwoFurious.java
@@ -19,8 +19,8 @@ public class TwoArmTwoFurious extends OpMode {
 
     private Follower follower;
     private final Pose startPose = new Pose(8, 72, 0);
-    private final Pose SpecGrab = new Pose(13, 35, Math.toRadians(180));
-    private final Pose Basket = new Pose(17, 125, Math.toRadians(130));
+    private final Pose SpecGrab = new Pose(11.5, 34.5, Math.toRadians(180));
+    private final Pose Basket = new Pose(14, 122.5, Math.toRadians(130));
     private final Pose HangLeft = new Pose( 80, 95, Math.toRadians(270));
     private final Pose HangRight = new Pose( 57, 95, Math.toRadians(270));
 
